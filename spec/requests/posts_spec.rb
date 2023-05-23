93e2b6176212ee4ac88cc13fcc3c7794197e8324
LeslieAine <aineleslie@gmail.com>
2023-05-23 18:34:18 +0300

    add users and posts controller specs

--- spec/requests/posts_spec.rb
+++ spec/requests/posts_spec.rb
@@ -1,7 +1,56 @@
-require 'rails_helper'
+require_relative '../rails_helper'
 
 RSpec.describe 'Posts', type: :request do
-  describe 'GET /index' do
-    pending "add some examples (or delete) #{__FILE__}"
+  describe 'GET /users/:id/posts' do
+    before do
+      User.create!(id: 1,
+                   name: 'Example User',
+                   photo: 'https://via.placeholder.com/150',
+                   bio: 'This is an example user bio.',
+                   posts_counter: 0)
+      get '/users/1/posts'
+    end
+
+    it 'returns a success response' do
+      expect(response).to have_http_status(200)
+    end
+
+    it 'renders the correct placeholder text' do
+      expect(response.body).to include('Placeholder for /users/:id/posts')
+    end
+
+    it 'renders the index template' do
+      expect(response).to render_template(:index)
+    end
+  end
+
+  describe 'GET /users/:id/posts/:id' do
+    before do
+      User.create!(id: 1,
+                   name: 'Example User',
+                   photo: 'https://via.placeholder.com/150',
+                   bio: 'This is an example user bio.',
+                   posts_counter: 0)
+
+      Post.create!(id: 1,
+                   title: 'Example Post',
+                   text: 'This is an example post body.',
+                   comments_counter: 0,
+                   likes_counter: 0,
+                   author_id: 1)
+      get '/users/1/posts/1'
+    end
+
+    it 'returns a success response' do
+      expect(response).to have_http_status(200)
+    end
+
+    it 'renders the correct placeholder text' do
+      expect(response.body).to include('Placeholder for /users/:id/posts/:post_id')
+    end
+
+    it 'renders the show template' do
+      expect(response).to render_template(:show)
+    end
   end
 end
--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -1,7 +1,44 @@
-require 'rails_helper'
+require_relative '../rails_helper'
 
 RSpec.describe 'Users', type: :request do
-  describe 'GET /index' do
-    pending "add some examples (or delete) #{__FILE__}"
+  describe 'GET /users' do
+    before do
+      get '/users'
+    end
+
+    it 'returns a success response' do
+      expect(response).to have_http_status(200)
+    end
+
+    it 'renders the correct placeholder text' do
+      expect(response.body).to include('Placeholder for /users and root')
+    end
+
+    it 'renders the index template' do
+      expect(response).to render_template(:index)
+    end
+  end
+
+  describe 'GET /users/:id' do
+    before do
+      User.create!(id: 1,
+                   name: 'Example User',
+                   photo: 'https://via.placeholder.com/150',
+                   bio: 'This is an example user bio.',
+                   posts_counter: 0)
+      get '/users/1'
+    end
+
+    it 'returns a success response' do
+      expect(response).to have_http_status(200)
+    end
+
+    it 'renders the correct placeholder text' do
+      expect(response.body).to include('Placeholder for /users/:id')
+    end
+
+    it 'renders the show template' do
+      expect(response).to render_template(:show)
+    end
   end
 end
