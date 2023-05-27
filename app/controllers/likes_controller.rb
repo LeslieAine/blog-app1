3b33e84e24dcf45eab23d49d5059b94145dbefbf
LeslieAine <aineleslie@gmail.com>
2023-05-27 16:42:33 +0300

    fix linters

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -3,11 +3,12 @@ class LikesController < ApplicationController
     @post = Post.find(params[:post_id])
     @user = current_user
     @like = Like.new(author: @user, post: @post)
-    
+
     if @like.save
-        flash[:success] = 'Liked'
-        redirect_to user_post_path(@user, @post)
-    else 
-        flash[:danger] = 'Like could not be created.'
+      flash[:success] = 'Liked'
+      redirect_to user_post_path(@user, @post)
+    else
+      flash[:danger] = 'Like could not be created.'
+    end
   end
 end
