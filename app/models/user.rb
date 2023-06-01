6095639c6f4e766a1e1e39c952ab535b35aa03e6
LeslieAine <aineleslie@gmail.com>
2023-06-01 09:54:29 +0300

    Make email confirmable

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,8 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
-  :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+         :recoverable, :rememberable, :validatable, :confirmable
   validates :name, :photo, :bio, :postscounter, presence: true
   validates :postscounter, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
 
